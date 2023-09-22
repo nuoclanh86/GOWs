c95fec6f0d7d37d25a3bb9bf2a45243970fceb51
tung.nguyenthanh5 <tung.nguyenthanh5@gameloft.com>
2023-09-22 15:43:12 +0700

    - add code for loading bar

--- Assets/RPGMonsterWave02PBR/Scripts/ScenesManager.cs
+++ Assets/RPGMonsterWave02PBR/Scripts/ScenesManager.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScenesManager : MonoBehaviour
 {
+    public GameObject loadingScene;
+    public Image loadingBarFill;
+
     public static ScenesManager Instance;
     private void Awake()
     {
         Instance = this;
     }
 
+    IEnumerator LoadSceneAsync(MAPSCENE scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene.ToString());
+        loadingScene.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+    }
+
     public void LoadScene(MAPSCENE scene)
     {
-        SceneManager.LoadScene(scene.ToString());
+        StartCoroutine(LoadSceneAsync(scene));
     }
 
     public void LoadNewGame()
