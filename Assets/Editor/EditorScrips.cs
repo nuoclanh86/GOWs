3dcb1836390990102519d956765c0bd6a2707565
tung.nguyenthanh5 <tung.nguyenthanh5@gameloft.com>
2023-09-23 09:51:03 +0700

    - add open scene btn for unity editor

--- Assets/Editor/EditorScrips.cs
+++ Assets/Editor/EditorScrips.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
- 
+
 class EditorScrips : EditorWindow
 {
- 
+    static string mainMenuPath = "Assets/_MainGame/Scenes/_MainMenu.unity";
+    static string level01Path = "Assets/_MainGame/Scenes/_Level01.unity";
+
     [MenuItem("Play/PlayMe _%h")]
     [System.Obsolete]
-    public static void RunMainScene()
+    public static void PlayFromPrelaunchScene()
     {
-        EditorApplication.OpenScene("Assets/_MainGame/Scenes/_MainMenu.unity");
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene(mainMenuPath);
         EditorApplication.isPlaying = true;
     }
+
+    [MenuItem("Play/OpenSceneLevel01 _%j")]
+    [System.Obsolete]
+    public static void OpenSceneLevel01()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            return;
+        }
+        EditorApplication.SaveCurrentSceneIfUserWantsTo();
+        EditorApplication.OpenScene(level01Path);
+    }
 }
- 
