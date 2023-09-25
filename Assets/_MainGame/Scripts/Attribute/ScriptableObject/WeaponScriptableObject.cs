9ae04ffdfb59d35091a2ef3c24033c2fa0c82baa
Tung Nguyen Thanh <tung.nguyenthanh5@gameloft.com>
2023-09-26 04:50:12 +0700

    - restruct ScriptableObject

---
+++ Assets/_MainGame/Scripts/Attribute/ScriptableObject/WeaponScriptableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewWeaponAttribute", menuName = "Assests/NewWeaponAttribute")]
+public class WeaponScriptableObject : AttributeScriptableObject
+{
+    public float cooldownAttribute;
+}
