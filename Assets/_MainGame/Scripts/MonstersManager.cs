7acb40d06d7953ccffc1d8c39f0053532376b919
Tung Nguyen Thanh <tung.nguyenthanh5@gameloft.com>
2023-09-26 14:34:26 +0700

    - add MapLevel ScriptableObject - update spawn monster manager

--- Assets/_MainGame/Scripts/MonstersManager.cs
+++ Assets/_MainGame/Scripts/MonstersManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MonstersManager : MonoBehaviour
 {
+    public MapLevelSO mapLevelSO;
+
+    Timer cooldownPerSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        cooldownPerSpawn = new Timer();
+        cooldownPerSpawn.SetDuration(mapLevelSO.cooldownPerSpawn);
+        SpawnMonster();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cooldownPerSpawn.IsDone())
+        {
+            SpawnMonster();
+        }
+        else
+        {
+            cooldownPerSpawn.Update(Time.deltaTime);
+        }
+    }
 
+    void SpawnMonster()
+    {
+        for (int i = 0; i < mapLevelSO.numbersMonsterPerSpawn; i++)
+        {
+            int randomIndex = Random.Range(0, mapLevelSO.monsters.Count);
+            Vector3 spawnPos = Vector3.zero;
+            GameObject monster = Instantiate(mapLevelSO.monsters[randomIndex], spawnPos, Quaternion.identity);
+            // monster.GetComponent<MonsterController>().MovingToTarget();
+        }
+        cooldownPerSpawn.Reset();
     }
 }
