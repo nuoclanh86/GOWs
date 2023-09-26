d8d6b34bdfed729fb5edff41e71d4ffcb8e41574
Tung Nguyen Thanh <tung.nguyenthanh5@gameloft.com>
2023-09-26 18:23:16 +0700

    - only spawn monster nearly player

--- Assets/_MainGame/Scripts/MonstersManager.cs
+++ Assets/_MainGame/Scripts/MonstersManager.cs
@@ -6,6 +6,8 @@ public class MonstersManager : MonoBehaviour
 {
     public MapLevelSO mapLevelSO;
     [SerializeField] List<GameObject> spawnAreas;
+    [SerializeField] float maxDistanceSpawn = 30.0f;
+    [SerializeField] float minDistanceSpawn = 3.0f;
 
     Timer cooldownPerSpawn;
 
@@ -29,24 +31,68 @@ public class MonstersManager : MonoBehaviour
             cooldownPerSpawn.Update(Time.deltaTime);
         }
     }
-
     void SpawnMonster()
     {
-        int randomSpawnAreaIndex = Random.Range(0, spawnAreas.Count);
-        Transform areaTransform = spawnAreas[randomSpawnAreaIndex].transform;
-        float minX = areaTransform.position.x - areaTransform.localScale.x / 2;
-        float maxX = areaTransform.position.x + areaTransform.localScale.x / 2;
-        float minZ = areaTransform.position.z - areaTransform.localScale.z / 2;
-        float maxZ = areaTransform.position.z + areaTransform.localScale.z / 2;
+        Vector3 playerPos = Vector3.zero;
+        if (ActionPhaseManager.GetInstance().GetMainPlayer())
+            playerPos = ActionPhaseManager.GetInstance().GetMainPlayer().transform.position;
+        float minX = playerPos.x - maxDistanceSpawn;
+        float maxX = playerPos.x + maxDistanceSpawn;
+        float minZ = playerPos.z - maxDistanceSpawn;
+        float maxZ = playerPos.z + maxDistanceSpawn;
+
+        Vector3 spawnPos = Vector3.zero;
         for (int i = 0; i < mapLevelSO.numbersMonsterPerSpawn; i++)
         {
             int randomIndex = Random.Range(0, mapLevelSO.monsters.Count);
-            Vector3 spawnPos = new Vector3(Random.Range(minX, maxX), 1.0f, Random.Range(minZ, maxZ));
+            spawnPos = new Vector3(Random.Range(minX, maxX), 1.0f, Random.Range(minZ, maxZ));
+            if (Vector3.Distance(spawnPos, playerPos) < minDistanceSpawn)
+            {
+                spawnPos += ((spawnPos - playerPos).normalized * minDistanceSpawn);
+            }
             GameObject monster = Instantiate(mapLevelSO.monsters[randomIndex], spawnPos, Quaternion.identity);
-            // monster.GetComponent<MonsterController>().MovingToTarget();
-
         }
         ActionPhaseManager.GetInstance().UpdateTotalMonsterOnScreen(mapLevelSO.numbersMonsterPerSpawn);
         cooldownPerSpawn.Reset();
     }
+    // void SpawnMonster()
+    // {
+    //     int randomSpawnAreaIndex = Random.Range(0, spawnAreas.Count);
+    //     Transform areaTransform = spawnAreas[randomSpawnAreaIndex].transform;
+    //     float minX = areaTransform.position.x - areaTransform.localScale.x / 2;
+    //     float maxX = areaTransform.position.x + areaTransform.localScale.x / 2;
+    //     float minZ = areaTransform.position.z - areaTransform.localScale.z / 2;
+    //     float maxZ = areaTransform.position.z + areaTransform.localScale.z / 2;
+
+    //     Vector3 playerPos = Vector3.zero;
+    //     if (ActionPhaseManager.GetInstance().GetMainPlayer())
+    //         playerPos = ActionPhaseManager.GetInstance().GetMainPlayer().transform.position;
+    //     int count, COUNTNUM = 5;
+    //     float minDistance = Mathf.Infinity, curDistance;
+    //     Vector3 spawnPos = Vector3.zero;
+    //     Vector3 nearestSpawnPos = spawnPos;
+    //     for (int i = 0; i < mapLevelSO.numbersMonsterPerSpawn; i++)
+    //     {
+    //         int randomIndex = Random.Range(0, mapLevelSO.monsters.Count);
+    //         spawnPos = new Vector3(Random.Range(minX, maxX), 1.0f, Random.Range(minZ, maxZ));
+    //         //random "count" times and take a pos nearest with player
+    //         count = COUNTNUM;
+    //         while (count > 0)
+    //         {
+    //             curDistance = Vector3.Distance(spawnPos, playerPos);
+    //             if (minDistance > curDistance)
+    //             {
+    //                 nearestSpawnPos = spawnPos;
+    //                 minDistance = curDistance;
+    //                 Debug.Log("nearestSpawnPos : " + nearestSpawnPos);
+    //             }
+    //             count--;
+    //         }
+    //         GameObject monster = Instantiate(mapLevelSO.monsters[randomIndex], spawnPos, Quaternion.identity);
+    //         // monster.GetComponent<MonsterController>().MovingToTarget();
+
+    //     }
+    //     ActionPhaseManager.GetInstance().UpdateTotalMonsterOnScreen(mapLevelSO.numbersMonsterPerSpawn);
+    //     cooldownPerSpawn.Reset();
+    // }
 }
