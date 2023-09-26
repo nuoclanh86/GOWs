bee82e4f2c24ca2969534266f8c000951b331913
Tung Nguyen Thanh <tung.nguyenthanh5@gameloft.com>
2023-09-27 06:46:27 +0700

    - add cheat HP for player

--- Assets/_MainGame/Scripts/ScriptableObject/MapLevelSO.cs
+++ Assets/_MainGame/Scripts/ScriptableObject/MapLevelSO.cs
@@ -8,10 +8,13 @@ public class MapLevelSO : ScriptableObject
     public List<GameObject> monsters;
     public float cooldownPerSpawn;
     public int numbersMonsterPerSpawn;
-    
+
     //distance from spawn pos to monster
     [Tooltip("Distance from spawnpos to mainplayer.")]
     public float maxDistanceSpawn = 30.0f;
     [Tooltip("Distance from spawnpos to mainplayer.")]
     public float minDistanceSpawn = 3.0f;
+
+    //cheat
+    public int cheatHPPlayer = 0;
 }
