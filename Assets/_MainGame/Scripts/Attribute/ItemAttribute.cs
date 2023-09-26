6df95542ee42a4311351b5c1f4cbbf1b499df335
Tung Nguyen Thanh <tung.nguyenthanh5@gameloft.com>
2023-09-26 11:47:02 +0700

    - just clearly debug log

--- Assets/_MainGame/Scripts/Attribute/ItemAttribute.cs
+++ Assets/_MainGame/Scripts/Attribute/ItemAttribute.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemAttribute : Attribute
-{  
+{
     protected virtual void OnTriggerEnter(Collider other)
     {
-        Debug.Log("OnTriggerEnter other : " + other.name);
+        Debug.Log("OnTriggerEnter other : " + other.name + " by " + this.name);
         if (other.tag == "Monster")
         {
             CharAttribute cA = other.GetComponent<CharAttribute>();
